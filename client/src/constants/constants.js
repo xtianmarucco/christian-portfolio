f6b9178e64bbe46a8b1ed73e0f0e6f379860fe9d
xtianmarucco <christian.marucco@gmail.com>
2023-09-12 01:00:59 -0300

    cv updated and changes.

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -39,5 +39,5 @@ export const TimeLineData = [
   { year: 2018, text: 'Started as a trainee at Circulo Agencia', },
   { year: 2019, text: 'Landed my first full-time gig as a front-end developer at Devlights', },
   { year: 2021, text: 'Started working at Baufest on a FinTech project', },
-  { year: 2023, text: 'Worked at Saffire, building themes and templates for their platform', },
+  { year: 2023, text: 'Working as freelance developer, building themes and templates, landing pages, SEO and analytics', },
 ];
