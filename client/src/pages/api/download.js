cbc01bc972abbb0b0d048b5ddf4bd14339437079
xtianmarucco <christian.marucco@gmail.com>
2023-10-04 16:24:23 -0300

    updated download feature

--- client/src/pages/api/download.js
+++ client/src/pages/api/download.js
@@ -2,12 +2,12 @@ import path from "path";
 import fs from "fs";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "src", "download", "CVMaruccoChristian.pdf");
+  const filePath = path.join(process.cwd(), "src", "download", "cvMaruccoGonzalezChristian.pdf");
   const stat = fs.statSync(filePath);
 
   res.setHeader("Content-Length", stat.size);
   res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Content-Disposition", "attachment; filename=CVMaruccoChristian.pdf");
+  res.setHeader("Content-Disposition", "attachment; filename=cvMaruccoGonzalezChristian.pdf");
 
   const fileStream = fs.createReadStream(filePath);
   fileStream.pipe(res);
